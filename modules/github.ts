e202546c9135b5c9bdc4dcfec724527cc788682f
cnoid <10182309+cnoid@users.noreply.github.com>
2023-11-16 16:55:14 +0100

    Update github.ts

--- modules/github.ts
+++ modules/github.ts
@@ -68,3 +68,56 @@ async function fetchGitHubData(userName: string): Promise<GitHubApiResponse> {
   const data = await res.json();
   return data.data;
 }
+
+
+// More data formatting, change as you need
+function transformGitHubData(data: GitHubApiResponse): TransformedData[] {
+  const result: TransformedData[] = [];
+
+  data.user.contributionsCollection.contributionCalendar.weeks.forEach(week => {
+    week.contributionDays.forEach(day => {
+      const date = new Date(day.date);
+      const count = day.contributionCount;
+      const month = date.toLocaleString('default', { month: 'long' });
+      const dayOfMonth = `${date.getDate()}${getOrdinalSuffix(date.getDate())}`;
+      const level = determineLevel(count);
+
+      result.push({ count, month, day: dayOfMonth, level });
+    });
+  });
+
+  return result;
+}
+
+// Helper function to return the ordinal suffix for a day
+function getOrdinalSuffix(day: number): string {
+  const j = day % 10,
+        k = day % 100;
+  if (j == 1 && k != 11) {
+      return "st";
+  }
+  if (j == 2 && k != 12) {
+      return "nd";
+  }
+  if (j == 3 && k != 13) {
+      return "rd";
+  }
+  return "th";
+}
+
+// Determine the contribution level
+function determineLevel(count: number): number {
+  if (count === 0) return 0;
+  if (count < 5) return 1;
+  if (count < 10) return 2;
+  if (count < 20) return 3; 
+  return 4; 
+}
+
+
+// Main function to retrieve and transform GitHub contribution data
+export async function retrieveContributionData(userName: string): Promise<TransformedData[]> {
+  const fetchedData = await fetchGitHubData(userName);
+  const transformedData = transformGitHubData(fetchedData);
+  return transformedData;
+}
