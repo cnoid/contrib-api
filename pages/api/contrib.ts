39168d877877aa761e76c3884c4e60d11d1cbc6b
cnoid <10182309+cnoid@users.noreply.github.com>
2023-11-16 21:33:41 +0100

    Update contrib.ts

--- pages/api/contrib.ts
+++ pages/api/contrib.ts
@@ -1,7 +1,28 @@
+import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { retrieveContributionData } from '../modules/github';
+import { retrieveContributionData } from '../../modules/github';
+
+// CORS
+const cors = Cors({
+  methods: ['GET', 'POST', 'OPTIONS'], // Adjust as needed
+});
+
+
+const runMiddleware = (req: NextApiRequest, res: NextApiResponse, fn: any) => {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await runMiddleware(req, res, cors); 
+  // CORS end
+
   try {
     const userName = req.query.userName as string;
     const contributionData = await retrieveContributionData(userName);
